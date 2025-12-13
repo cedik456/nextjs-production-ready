207ffb5d1bd1e1316457df0440a066d81a60d96d
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-12-13 18:13:56 +0800

    Finish theories
    Let's codee

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,24 @@
-const Home = () => {
+type Album = {
+  id: number;
+  title: string;
+};
+
+const Home = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/albums");
+  if (!response.ok) throw new Error("Failed to fetch data");
+
+  const albums: Album[] = await response.json();
   return (
-    <div className="text-red-500">
-      <p>Hello</p>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols gap-4">
+      {albums.map((album) => (
+        <div
+          key={album.id}
+          className="bg-white shadow-md rounded-lg p-4 text-black "
+        >
+          <h3 className="text-lg font-bold mb-2">{album.title}</h3>
+          <p className="text-gray-600 ">Album ID: {album.id}</p>
+        </div>
+      ))}
     </div>
   );
 };
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
+
   experimental: {
     turbopackFileSystemCacheForDev: true,
   },
