82b9020b38a73129202ac552b77b8fd1bf1798c1
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-12-16 12:31:53 +0800

    complete homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
+import { events } from "@/lib/constants";
 
 const Home = () => {
   return (
@@ -12,6 +14,18 @@ const Home = () => {
       </p>
 
       <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3 className="">Feature Events</h3>
+
+        <ul className="events">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
